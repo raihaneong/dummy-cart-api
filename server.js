fdbe8bdd5dd4fd1f4efc3dd0c10c54369f0ad4c6
raihaneong <raihneong@gmail.com>
2023-02-26 22:35:42 +0700

    Get data product from stripe

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 
+// sk_test_51KyuwzK6L9E8BuRsmCOP54PoJUTsPfD9npTgfoOIeTve5gkBEUQXV63Bqux9KLxwtN1l5mBB5P9NjJcDukjgxovq00eRD0oHU1
+// Tea : price_1MfmARK6L9E8BuRsT76In9Qi
+// Coffe : price_1MfmBYK6L9E8BuRsSoMl2f0e
+// Sunglasses : price_1MfmC9K6L9E8BuRsq5ZDjuKF
+// Laptop : price_1MfmD8K6L9E8BuRsRyaGJ2be
+// Camera : price_1MfmDoK6L9E8BuRszIcubN8P
+// Bag : price_1MfmESK6L9E8BuRsN60BLMe1
